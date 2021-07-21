807985066d68f1928fb501fc783e64c5fd5e93a8
Aaron Oliver-Taylor <aaron.oliver-taylor@goldstandardphantoms.com>
2021-07-21 18:06:59 +0100

    added single PLD ASLDRO generation (#4)

--- src/renaldro/test_main.py
+++ src/renaldro/test_main.py
@@ -1,14 +1,16 @@
 import sys
+import numpy as np
 import json
 import os
 from asldro.filters.json_loader import JsonLoaderFilter
 from asldro.filters.nifti_loader import NiftiLoaderFilter
+from asldro.filters.transform_resample_image_filter import TransformResampleImageFilter
 import pytest
 from unittest.mock import patch
 from tempfile import TemporaryDirectory
 import nibabel as nib
 import numpy.testing
-from renaldro.main import main, generate_renal_ground_truth
+from renaldro.main import main, generate_renal_ground_truth, generate_asldro_params
 from renaldro.data.filepaths import DATA_DIR
 
 
@@ -55,11 +57,15 @@ def test_generate_renal_ground_truth():
                     "seg_label",
                 ],
                 "units": ["ml/100g/min", "s", "s", "s", "s", "", ""],
-                "segmentation": {"background": 0, "cortex": 1, "medulla": 2,},
+                "segmentation": {
+                    "background": 0,
+                    "cortex": 1,
+                    "medulla": 2,
+                },
                 "parameters": {
                     "lambda_blood_brain": 0.9,
                     "magnetic_field_strength": 3.0,
-                    "t1_arterial_blood": 1.2,
+                    "t1_arterial_blood": 1.65,
                 },
             }
             assert json_loader.outputs == out[hrgt]["image_info"]
@@ -67,6 +73,15 @@ def test_generate_renal_ground_truth():
             numpy.testing.assert_array_equal(
                 nifti_loader.outputs["image"].image, out[hrgt]["image"].image
             )
+
+            assert os.path.exists(os.path.join(temp_dir, "dro_out_ss_hrgt_simple.zip"))
+            assert os.path.exists(
+                os.path.join(temp_dir, "dro_out_ss_hrgt_real_rbf.zip")
+            )
+
+            asl_simple = out["dro_out_hrgt_simple"]["asldro_output"][0]
+
+            gt_m0 = out["dro_out_hrgt_simple"]["hrgt"]["m0"]
 
 
 def test_main_cli():
@@ -76,3 +91,19 @@ def test_main_cli():
         with patch.object(sys, "argv", testargs):
             main()
 
+
+def test_generate_asldro_params():
+    """Tests the single subtraction parameter generation function"""
+
+    hrgt = {
+        "nifti": "path/to/hrgt.nii.gz",
+        "json": "path/to/hrgt.json",
+        "name": "random hrgt",
+    }
+    params = generate_asldro_params(hrgt)
+
+    assert params["global_configuration"]["ground_truth"] == {
+        "nii": "path/to/hrgt.nii.gz",
+        "json": "path/to/hrgt.json",
+    }
+    assert params["image_series"][0]["series_type"] == "asl"
